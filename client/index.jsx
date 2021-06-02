9a8bcfa1c9fa16d9073b8eab9c7f6bcb3268fd7b
raphaelwho <raphael-lhs@hotmail.com>
2021-06-02 13:28:19 -0500

    add button component

--- client/index.jsx
+++ client/index.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import text, {CText, CTextDemoView} from './components/shared/text.jsx';
-
+import Square_button from './components/shared/Square_button/Square_button.jsx'
 class App extends React.Component {
   constructor() {
     super();
   }
 
+
   render() {
-    return <h1>Hi</h1>
+    return <h1>Hi </h1>
+
+
+
 
   }
 }
