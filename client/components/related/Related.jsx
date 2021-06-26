cf8a43c8efb98fa18045abf14518e346f32e7f66
raphaelwho <raphael-lhs@hotmail.com>
2021-06-26 14:08:15 -0500

    bug

--- client/components/related/Related.jsx
+++ client/components/related/Related.jsx
@@ -32,8 +32,8 @@ export default class Related extends Component {
     this.setState({myoutfits});
     log(this.props.id);
     axios.all([
-      axios.post(`/card`, {id: this.props.id}),
-      axios.post(`/related`, {id: this.props.id}),
+      axios.post(`${process.env.EXPRESS_SERVER}/card`, {id: this.props.id}),
+      axios.post(`${process.env.EXPRESS_SERVER}/related`, {id: this.props.id}),
     ]).then(axios.spread((data1, data2) => {
       this.setState({
         cur: data1.data,
@@ -45,8 +45,8 @@ export default class Related extends Component {
   componentDidUpdate(prevProps){
     if (this.props.id!== prevProps.id){
       axios.all([
-        axios.post('/card', {id: this.props.id}),
-        axios.post('/related', {id: this.props.id}),
+        axios.post(`${process.env.EXPRESS_SERVER}/card`, {id: this.props.id}),
+        axios.post(`${process.env.EXPRESS_SERVER}/card`, {id: this.props.id}),
       ]).then(axios.spread((data1, data2) => {
         this.setState({
           cur: data1.data,
