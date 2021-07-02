28b376132de37142d19db033f9bb2817a1bfe72c
andrew <handrew881@gmail.com>
2021-07-02 16:38:49 -0400

    Bottom Details

--- client/components/product/Product.jsx
+++ client/components/product/Product.jsx
@@ -6,6 +6,7 @@ import './css/Product.css'
 import AddToCart from './AddToCart.jsx'
 import ImageGallery from './ImageGallery.jsx'
 import { getActiveProductInfo, getActiveProductStyles } from '../../serverHelpers/productServerHelper.js'
+import BottomDetails from './BottomDetails.jsx'
 
 class Product extends React.Component {
     constructor(props) {
@@ -30,6 +31,7 @@ class Product extends React.Component {
 
     updateComponent() {
         getActiveProductInfo(this.props.id).then((data) => {
+            log(data)
             this.setState({activeProduct: data});
         });
         getActiveProductStyles(this.props.id).then((data) => {
@@ -56,17 +58,22 @@ class Product extends React.Component {
 
     render() {
         return(
-            <div className="product-master-grid" style={this.state.fullscreen ? {gridTemplateColumns: "2fr 0fr"}:{gridTemplateColumns: "2fr 1fr"}}>
-                <div className="leading" >
-                    <ImageGallery product={this.state.selectedStyle} stylesIdx={this.state.selectedStyleIdx} styles={this.state.styles} screenToggle={() => {this.viewToggle()}} switchStyle={(idx) => {this.switchStyle(idx)}} fullscreen={this.state.fullscreen}/>
+            <div className="product">
+                <div className="product-master-grid" style={this.state.fullscreen ? {gridTemplateColumns: "2fr 0fr"}:{gridTemplateColumns: "2fr 1fr"}}>
+                    <div className="leading" >
+                        <ImageGallery product={this.state.selectedStyle} stylesIdx={this.state.selectedStyleIdx} styles={this.state.styles} screenToggle={() => {this.viewToggle()}} switchStyle={(idx) => {this.switchStyle(idx)}} fullscreen={this.state.fullscreen}/>
+                    </div>
+
+                    <div className="trailing" style={this.state.fullscreen ? {display: "none"}:{gridTemplateColumns: "inline-block"}}>
+                        <ProductInfo product={this.state.activeProduct}/>
+                        <StyleSelector styles={this.state.styles} switchStyle={(idx) => {this.switchStyle(idx)}} stylesIdx={this.state.selectedStyleIdx}/>
+                        <AddToCart product={this.state.selectedStyle}/>
+                    </div>
                 </div>
 
-                <div className="trailing" style={this.state.fullscreen ? {display: "none"}:{gridTemplateColumns: "inline-block"}}>
-                    <ProductInfo product={this.state.activeProduct}/>
-                    <StyleSelector styles={this.state.styles} switchStyle={(idx) => {this.switchStyle(idx)}} stylesIdx={this.state.selectedStyleIdx}/>
-                    <AddToCart product={this.state.selectedStyle}/>
-                </div>
+                <BottomDetails product={this.state.activeProduct}></BottomDetails>
             </div>
+
         )
     }
 }
